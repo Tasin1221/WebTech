34e431c3b7d8445eba0a89cc0b36fd4f991e1f42
Tasin1221 <65084397+Tasin1221@users.noreply.github.com>
2021-02-20 02:21:36 +0600

    Form Basic

--- Cv/Form.php
+++ Cv/Form.php
@@ -4,19 +4,19 @@
 	<title></title>
 </head>
 <body>
-	<table border="5">
+<!--	<table border="5" align="center">
 		<tr>
-			<td>ID</td>
-			<td>NAME</td>
-			<td>CGPA</td>
+			<td><b>ID</b></td>
+			<td><b>NAME</b></td>
+			<td><b>GPA</b></td>
 		</tr>
 		<tr>
-			<td>1</td>
+			<td rowspan="2">1</td>
 			<td>Rahim</td>
 			<td>3.76</td>
 		</tr>
 		<tr>
-			<td>2</td>
+			
 			<td>Karim</td>
 			<td>3.45</td>
 		</tr>
@@ -27,6 +27,24 @@
 		</tr>
 		
 	</table>
+-->
+
+
+
+<h1 align="center">User Registration</h1>
+<form align="center">
+	<span>UserName</span>:<input type="text"><br>
+	<span>Password</span>:<input type="Password"><br>
+	<span>Gender</span>:<input type="radio" name="Gender">Male <input type="radio" name="Gender">Female <br>
+	<span>Hobbies</span>:<input type="checkbox" >Music <input type="checkbox" >Movies <input type="checkbox" >Playing <br>
+	<span>Profession:</span> 
+	<select>
+		<option>Teaching</option>
+		<option>Business</option>
+		<option>Service</option>
+	</select><br>
+	<input type="submit" value ="submit">
+</form>
 
 </body>
 </html>

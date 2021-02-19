1bd5cc11c12570082a161437e85dedc053c2f3fa
Tasin1221 <65084397+Tasin1221@users.noreply.github.com>
2021-02-20 00:10:42 +0600

    done

---
+++ Cv/Form.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<table border="5">
+		<tr>
+			<td>ID</td>
+			<td>NAME</td>
+			<td>CGPA</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>Rahim</td>
+			<td>3.76</td>
+		</tr>
+		<tr>
+			<td>2</td>
+			<td>Karim</td>
+			<td>3.45</td>
+		</tr>
+		<tr>
+			<td>3</td>
+			<td>Maruf</td>
+			<td>3.12</td>
+		</tr>
+		
+	</table>
+
+</body>
+</html>
